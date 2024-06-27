4bacb3e159cb34060c9b08b79fe535204960673e
plamkutryanska <plamietsy@gmail.com>
2024-06-27 10:01:59 -0500

    Making small changes, still using mockData due to network errors when trying to fetch from the Rest API provided

--- src/app/components/CountyCapitalMatch.js
+++ src/app/components/CountyCapitalMatch.js
@@ -1,56 +1,20 @@
 "use client"
 import {useState, useEffect} from 'react'
+import Link from 'next/link'
 import { isSelectionCorrect, removeData } from '../utils/countryCapitalUtils'
+import { mockData } from '../data/mockData'
 import styles from '../components/countryCapitalMatch.module.css'
 import _ from 'lodash'
-import Link from 'next/link'
-
-const mockData = [
-  {
-      "name": "Austria",
-      "capital": "Vienna"
-  },
-  // {
-  //     "name": "Belgium",
-  //     "capital": "Brussels"
-  // },
-  {
-      "name": "Bulgaria",
-      "capital": "Sofia"
-  },
-  // {
-  //     "name": "France",
-  //     "capital": "Paris"
-  // },
-  // {
-  //     "name": "Germany",
-  //     "capital": "Berlin"
-  // },
-  // {
-  //     "name": "Italy",
-  //     "capital": "Rome"
-  // },
-  // {
-  //     "name": "Netherlands",
-  //     "capital": "Amsterdam"
-  // },
-  // {
-  //     "name": "Poland",
-  //     "capital": "Warsaw"
-  // }
-]
 
 const CountryCapitalMatch = () => {
   const [shuffledCountries, setShuffledCountries] = useState([])
   const [shuffledCapitals, setShuffledCapitals] = useState([])
 
   const [correctMatches, setCorrectMatches] = useState([])
-
   const [selectedPair, setSelectedPair] = useState([])
-  const [score, setScore] = useState(30)
   const [matchIsIncorrect, setMatchIsIncorrect] = useState(false)
 
-  const [isPairCorrect, setIsPairCorrect] = useState(false)
+  const [score, setScore] = useState(30)
 
   const buttonIsDisabled = selectedPair.length < 1
 
@@ -60,47 +24,50 @@ const CountryCapitalMatch = () => {
     setShuffledCapitals(_.shuffle(mockData))
   }, [])
 
+  const calculteScore = (selectedCapital) => {
+    if(_.isEqual(selectedCapital, selectedPair[0])){
+      setScore(score + 7)
+    } else {
+      setScore(score - 5)
+    }
+  }
+
 
   const handleSelectedCapital = (selectedCapitalData) => {
     setSelectedPair((prv) => [...prv, selectedCapitalData])
 
     if(_.isEqual(selectedCapitalData, selectedPair[0])){
-      setScore(score + 7)
       setMatchIsIncorrect(false)
-      setIsPairCorrect(true) //NEW
 
       setCorrectMatches((prvCorrectMatch) => [...prvCorrectMatch, selectedCapitalData])
+
       setTimeout(() => {
         setShuffledCapitals((prv) => removeData(prv, selectedCapitalData))
         setShuffledCountries((prv) => removeData(prv, selectedCapitalData))
 
       }, 1000)
     } else {
-      setScore(score - 5)
       setMatchIsIncorrect(true)
-      setIsPairCorrect(false) //NEW
-
     }
     setTimeout(()=> {
       setSelectedPair([])
       setMatchIsIncorrect(false)
-      setIsPairCorrect(true) //NEW
     }, 1000)
   }
 
-
-const ButtonComponent = ({data, onClick, pairIndex, objectKey}) => {
+const ButtonComponent = ({data, onClick, pairIndex, objectKey, isDisabled}) => {
+  const style = `${styles.button} 
+    ${isSelectionCorrect(data, selectedPair) && styles.correctSelection}
+    ${_.isEqual(selectedPair[pairIndex], data) && styles.activeSelection}
+    ${(matchIsIncorrect && _.isEqual(selectedPair[pairIndex], data)) && styles.incorrectSelection}
+    ${isDisabled && styles.disabled}
+  `
   return (
     <button 
       onClick={onClick}
       key={data[objectKey]}
-      className={
-        `${styles.button} 
-        ${isSelectionCorrect(data, selectedPair) && styles.correctSelection}
-        ${_.isEqual(selectedPair[pairIndex], data) && styles.activeSelection}
-        ${(matchIsIncorrect && _.isEqual(selectedPair[pairIndex], data)) && styles.incorrectSelection}
-        `
-      }
+      className={style}
+      disabled={isDisabled}
     > 
       {data[objectKey]}
     </button>
@@ -112,16 +79,25 @@ const CountryCapitlLists = () => {
       <div style={{display: 'flex'}}>
       <div className={styles.buttonContainer}>
       {shuffledCountries.map((data) => {
-        return (
-          <ButtonComponent data={data} pairIndex={0} onClick={() => setSelectedPair([data])} objectKey={'name'}/>
-        )
+        return <ButtonComponent 
+                  data={data}
+                  pairIndex={0}
+                  onClick={() => setSelectedPair([data])}
+                  objectKey={'name'}
+                  key={data.name}
+                  />
       })}
       </div>
       <div className={styles.buttonContainer}>
       {shuffledCapitals.map((data) => {
-        return (
-          <ButtonComponent data={data} pairIndex={1} onClick={() => handleSelectedCapital(data)} objectKey={'capital'}/>
-        )
+        return <ButtonComponent 
+                data={data}
+                pairIndex={1}
+                onClick={() => {handleSelectedCapital(data), calculteScore(data)}}
+                objectKey={'capital'}
+                key={data.capital}
+                isDisabled={buttonIsDisabled}
+                />
       })}
       </div>
     </div>
@@ -136,9 +112,9 @@ const CountryCapitlLists = () => {
         <CountryCapitlLists/> : 
         <Link href={{pathname: '/results', query: {score: score}}} className={styles.link}>
           View Results
-        </Link>}
+        </Link>
+      }
     </div>
-
   )
 }
 
--- src/app/game/page.js
+++ src/app/game/page.js
@@ -1,7 +1,8 @@
 import CountryCapitalMatch from '../components/CountyCapitalMatch'
 import styles from '../components/countryCapitalMatch.module.css'
 
-export default function Page() {
+export default async function Page() {
+
   return (
     <div className={styles.container}>
       <CountryCapitalMatch/>
